759625e1634d078b2b2efc2f2c82aaba489faae1
Shawn Catanzarite <me@shawncatz.com>
2013-08-24 13:10:56 -0700

    v0.1.5

--- lib/mystro/common/version.rb
+++ lib/mystro/common/version.rb
@@ -3,7 +3,7 @@ module Mystro
     module Version
       MAJOR = 0
       MINOR = 1
-      TINY  = 4
+      TINY  = 5
       TAG   = nil
       STRING = [MAJOR, MINOR, TINY, TAG].compact.join(".")
     end
