0b89a5e1c628d7c253b3aed5a7d7ee3852733f47
Shawn Catanzarite <me@shawncatz.com>
2013-08-24 15:19:23 -0700

    revert new dsl creeping in when it wasnt supposed to

--- lib/mystro/dsl/balancer/sticky.rb
+++
@@ -1,8 +0,0 @@
-module Mystro
-  module DSL
-      class Sticky < Mystro::DSL::Base
-        attribute :type
-        attribute :arg
-      end
-  end
-end
--- lib/mystro/dsl/template_file.rb
+++
@@ -1,7 +0,0 @@
-module Mystro
-  module DSL
-    class TemplateFile < Base
-      has_one :template
-    end
-  end
-end
